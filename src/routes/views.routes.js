8c6791c220b8bcf919486f90ea55cec24456e3d5
Lijo Hernan <lijo.hernan@gmail.com>
2024-06-18 00:51:39 -0300

    faltan estilos

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -10,14 +10,6 @@ router.get("/", async (req, res)=>{
     
     res.render("home")
     
-    // const {limit} = req.query;
-    // const prods= "home"
-
-    // if(limit){
-    //     res.render(prods.slice(0, limit));
-    // }else {
-    //     res.render(prods)
-    // }
 })
 
 export default router
